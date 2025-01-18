692c20ee6218af4911b616ad8a7efc3a6f164b3f
ju <121565439+julhias@users.noreply.github.com>
2025-01-18 16:11:03 -0300

    Update Game.py
    NEGAMAX Implementation

--- Game.py
+++ Game.py
@@ -1,44 +1,77 @@
 class Game:
+    def __init__(self):
+        self.winner = None
 
-	def __init__(self):
-		self.winner = None
+    # Função de avaliação simples
+    def evaluate(self, board):
+        red_piece, black_piece = self.check_piece(board)
+        return red_piece - black_piece
 
-	# checks if both colors still has a piece
-	def check_piece(self, board):
-		red_piece = 0
-		black_piece = 0
-		for y in range(board.board_size):
-			for x in range(board.board_size):
-				tile = board.get_tile_from_pos((x, y))
-				if tile.occupying_piece != None:
-					if tile.occupying_piece.color == "red":
-						red_piece += 1
-					else:
-						black_piece += 1
-		return red_piece, black_piece
+    # Algoritmo Negamax
+    def negamax(self, board, depth, alpha, beta, color):
+        if depth == 0 or self.is_game_over(board):
+            return color * self.evaluate(board), None
 
-	def is_game_over(self, board):
-		red_piece, black_piece = self.check_piece(board)
-		if red_piece == 0 or black_piece == 0:
-			self.winner = "red" if red_piece > black_piece else "black"
-			return True
-		else:
-			return False
+        max_eval = float('-inf')
+        best_move = None
 
-	def check_jump(self, board):
-		piece = None
-		for tile in board.tile_list:
-			if tile.occupying_piece != None:
-				piece = tile.occupying_piece
-				if len(piece.valid_jumps()) != 0 and board.turn == piece.color:
-					board.is_jump = True
-					break
-				else:
-					board.is_jump = False
-		if board.is_jump:
-			board.selected_piece = piece
-			board.handle_click(piece.pos)
-		return board.is_jump
+        # Gerar todos os movimentos válidos para o jogador atual
+        moves = self.generate_all_moves(board, color)
 
-	def message(self):
-		print(f"{self.winner} Wins!!")
+        for move in moves:
+            # Fazer o movimento
+            board_copy = self.simulate_move(board, move)
+            eval_score, _ = self.negamax(board_copy, depth - 1, -beta, -alpha, -color)
+            eval_score = -eval_score
+
+            # Desfazer o movimento (simulado no tabuleiro copiado)
+            if eval_score > max_eval:
+                max_eval = eval_score
+                best_move = move
+
+            alpha = max(alpha, eval_score)
+            if alpha >= beta:
+                break
+
+        return max_eval, best_move
+
+    def generate_all_moves(self, board, color):
+        moves = []
+        for tile in board.tile_list:
+            if tile.occupying_piece and tile.occupying_piece.color == ("red" if color == 1 else "black"):
+                moves.extend(tile.occupying_piece.valid_moves())
+                for jump in tile.occupying_piece.valid_jumps():
+                    moves.append(jump[0])  # Adiciona apenas a posição final dos saltos
+        return moves
+
+    def simulate_move(self, board, move):
+        import copy
+        board_copy = copy.deepcopy(board)
+        piece = board_copy.get_tile_from_pos(move[0].pos).occupying_piece
+        piece._move(move)  # Simula o movimento
+        return board_copy
+
+    # Verificar vencedores
+    def check_piece(self, board):
+        red_piece = 0
+        black_piece = 0
+        for y in range(board.board_size):
+            for x in range(board.board_size):
+                tile = board.get_tile_from_pos((x, y))
+                if tile.occupying_piece is not None:
+                    if tile.occupying_piece.color == "red":
+                        red_piece += 1
+                    else:
+                        black_piece += 1
+        return red_piece, black_piece
+
+    def is_game_over(self, board):
+        red_piece, black_piece = self.check_piece(board)
+        if red_piece == 0 or black_piece == 0:
+            self.winner = "red" if red_piece > black_piece else "black"
+            return True
+        else:
+            return False
+
+    def message(self):
+        print(f"{self.winner} Wins!!")
